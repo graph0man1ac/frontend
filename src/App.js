633f90b1cdadcfd14c90c9a6abdae81223826755
graph0man1ac <aidarin607@gmail.com>
2024-08-19 19:34:29 +0500

    front add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [likesCount, setLikesCount] = useState(0);
+
+  const getLikes = () => {
+    fetch("http://localhost:3000/likes/get", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setLikesCount(data[0].value))
+      .catch((error) => console.error("Ошибка синхронизации лайков", error));
+  };
+
+  useEffect(() => {
+    getLikes();
+    const intervalId = setInterval(() => {
+      getLikes();
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleOnClick = () => {
+    setLikesCount(likesCount + 1);
+    fetch("http://localhost:3000/likes/update", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          console.error("Ошибка обновления лайков");
+        }
+      })
+      .catch((error) => console.error("Ошибка обновления лайков", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        likes_counter
+        <Input likesCount={likesCount} />
+        <Button onClick={handleOnClick} name="Like" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+
+export const Input = (props) => {
+    
+  return <div>
+    <input value={props.likesCount}></input>
+  </div>;
+};
